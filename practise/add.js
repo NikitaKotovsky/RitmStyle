7750e71f42fe31f763f482f7fe2b42ac39a0de36
NikitaKotovsky <98867270+NikitaKotovsky@users.noreply.github.com>
2022-04-14 22:38:43 +0800

    Add files via upload

---
+++ practise/add.js
@@ -0,0 +1,8 @@
+//const header = document.querySelector("#header");
+//const header = document.querySelectorALL("#nav .li");
+
+function sayHello() {
+    console.log('I am able to so anithing!')
+}
+
+sayHello();
